b8426a5f22447fbad9066c79b7e5cb0d385b1b52
Will Rowston <willrowston@gmail.com>
2020-04-28 16:12:19 -0400

    created user router

---
+++ controllers/userRouter.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const UserModel = require('../models/user.js')
+
+const userRouter = express.Router()
+
+// GET ALL USERS
+userRouter.get('/', async (req, res) => {
+    try {
+        const allUsers = await UserModel.getAllUsers()
+        console.log('got all users successfully')
+        res.json(allUsers)
+    } catch (err) {
+        console.log(err)
+        res.json(err)
+    }
+})
+
+// GET A SINGLE USER
+userRouter.get('/:id', async (req, res) => {
+    try {
+        const singleUser = await UserModel.getOneUser()
+        console.log('got a single user successfully')
+        res.json(singleUser)
+    } catch (err) {
+        console.log(err)
+        res.json(err)
+    }
+})
+
+// CREATE A NEW USER
+userRouter.post('/', async (req, res) => {
+    try {
+        await UserModel.createUser(req.body)
+        res.json('created user')
+    } catch (err) {
+        console.log(err)
+        res.json(err)
+    }
+})
+
+// DELETE A USER
+userRouter.delete('/:id', async (req, res) => {
+    try {
+        await UserModel.deleteUser(req.params.id)
+        res.json('user deleted')
+    } catch (err) {
+        console.log(err)
+        res.json(err)
+    }
+})
+
+// UPDATE A USER
+userRouter.put('/:id', async (req, res) => {
+    try {
+        await UserModel.updateUser(req.params.id, req.body)
+        res.json('updated user')
+    } catch (err) {
+        console.log(err)
+        res.json(err)
+    }
+})
+
+module.exports = userRouter
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const userRouter = require('./controllers/userRouter.js')
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/user', userRouter)
 
 const PORT = process.env.PORT || 3000
 
