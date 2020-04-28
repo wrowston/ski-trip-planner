308dc602a539492bcfbedd0acf6a7d115250be8c
Will Rowston <willrowston@gmail.com>
2020-04-28 11:45:57 -0400

    created server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`App is listening on PORT ${PORT}`)
+})
