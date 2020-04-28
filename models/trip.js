f46ec510096248bc0f76ddd988b86de3ea083533
Will Rowston <willrowston@gmail.com>
2020-04-28 15:08:13 -0400

    created trip model

---
+++ models/trip.js
@@ -0,0 +1,44 @@
+const mongoose = require('../db/connection.js')
+const Schema = mongoose.Schema
+// require ski resort schema
+
+const tripSchema = new Schema({
+    name: String,
+    country: String,
+    state: String,
+    city: String,
+    airport: String,
+    daysSkiing: Number,
+    passortRequired: Boolean,
+    // skiResort: 
+})
+
+const tripCollection = mongoose.model('Trip', tripSchema)
+
+function getAllTrips() {
+    return tripCollection.find()
+}
+
+function getOneTrip(id) {
+    return tripCollection.findById(id)
+}
+
+function createTrip(newTrip) {
+    return tripCollection.create(newTrip)
+}
+
+function deleteTrip(id) {
+    return tripCollection.findByIdAndDelete(id)
+}
+
+function updateTrip(id, newTrip) {
+    return tripCollection.findByIdAndUpdate(id, newTrip)
+}
+
+module.exports = {
+    getAllTrips,
+    getOneTrip,
+    createTrip,
+    deleteTrip,
+    updateTrip
+}
