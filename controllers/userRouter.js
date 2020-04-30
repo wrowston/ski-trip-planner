04cf94f73896e62922fe59507746f7a86f450615
Will Rowston <willrowston@gmail.com>
2020-04-30 10:16:44 -0400

    refactored user controller

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const UserModel = require('../models/user.js')
+const userModel = require('../models/user.js')
 const tripModel = require('../models/trip.js')
 
 const userRouter = express.Router()
@@ -7,7 +7,7 @@ const userRouter = express.Router()
 // GET ALL USERS
 userRouter.get('/', async (req, res) => {
     try {
-        const allUsers = await UserModel.getAllUsers()
+        const allUsers = await userModel.getAllUsers()
         console.log('got all users successfully')
         res.render('user/allUsers', { allUsers })
     } catch (err) {
@@ -24,7 +24,7 @@ userRouter.get('/new', (req, res) => {
 // EDIT USER FORM
 userRouter.get('/:id/edit', async (req, res) => {
     try {
-        const singleUser = await UserModel.getOneUser(req.params.id)
+        const singleUser = await userModel.getOneUser(req.params.id)
         res.render('user/editUser', { singleUser })
     } catch (err) {
         console.log(err)
@@ -37,7 +37,7 @@ userRouter.get('/:id/edit', async (req, res) => {
 userRouter.get('/:id', async (req, res) => {
     console.log('userRouter.GET one route')
     try {
-        const singleUser = await UserModel.getOneUser(req.params.id)
+        const singleUser = await userModel.getOneUser(req.params.id)
         const trip = await tripModel.getAllTripsByUserId(req.params.id)
         console.log('got a single user successfully')
         res.render('user/singleUser', { singleUser, trip })
@@ -50,7 +50,7 @@ userRouter.get('/:id', async (req, res) => {
 // CREATE A NEW USER
 userRouter.post('/', async (req, res) => {
     try {
-        await UserModel.createUser(req.body)
+        await userModel.createUser(req.body)
         res.redirect('/user')
     } catch (err) {
         console.log(err)
@@ -61,7 +61,7 @@ userRouter.post('/', async (req, res) => {
 // DELETE A USER
 userRouter.delete('/:id', async (req, res) => {
     try {
-        await UserModel.deleteUser(req.params.id)
+        await userModel.deleteUser(req.params.id)
         res.redirect('/user')
     } catch (err) {
         console.log(err)
@@ -72,7 +72,7 @@ userRouter.delete('/:id', async (req, res) => {
 // UPDATE A USER
 userRouter.put('/:id', async (req, res) => {
     try {
-        await UserModel.updateUser(req.params.id, req.body)
+        await userModel.updateUser(req.params.id, req.body)
         res.redirect(`/user/${req.params.id}`)
     } catch (err) {
         console.log(err)
