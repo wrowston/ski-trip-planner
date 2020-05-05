790f6a6781417c3956afbb8485e58d3409d13838
Will Rowston <willrowston@gmail.com>
2020-05-05 16:48:55 -0400

    Updated DB connection code to use MongoLab when in Production

--- db/connection.js
+++ db/connection.js
@@ -2,13 +2,19 @@ const mongoose = require('mongoose');
 
 const connectionString = process.env.MONGODB_URI || "mongodb://localhost/skiTripCollection";
 
-mongoose.connect(connectionString)
-    .then(() => {
-        console.log("connected to mongo at: " + connectionString);
-    })
-    .catch((err) => {
-        console.log('Failed to connect to mongo')
-        console.log(err)
-    })
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+    mongoose.connect('mongodb://localhost/skiTripCollection');
+}
+mongoose.connection.on('error', err => {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+}
+);
+mongoose.connection.once('open', () => {
+    console.log("Mongoose has connected to MongoDB!");
+});
 
 module.exports = mongoose
