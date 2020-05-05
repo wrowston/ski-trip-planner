2e5f4630f265f79a76ee0411d1ddd19b3f41e6c5
Will Rowston <willrowston@gmail.com>
2020-05-05 17:26:44 -0400

    edited database connection

--- db/connection.js
+++ db/connection.js
@@ -2,19 +2,13 @@ const mongoose = require('mongoose');
 
 const connectionString = process.env.MONGODB_URI || "mongodb://localhost/skiTripCollection";
 
-if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI);
-}
-else {
-    mongoose.connect('mongodb://localhost/skiTripCollection');
-}
-mongoose.connection.on('error', err => {
-    console.error('MongoDB connection error: ' + err);
-    process.exit(-1);
-}
-);
-mongoose.connection.once('open', () => {
-    console.log("Mongoose has connected to MongoDB!");
-});
+mongoose.connect(connectionString)
+    .then(() => {
+        console.log("connected to mongo at: " + connectionString);
+    })
+    .catch((err) => {
+        console.log('Failed to connect to mongo')
+        console.log(err)
+    })
 
 module.exports = mongoose
